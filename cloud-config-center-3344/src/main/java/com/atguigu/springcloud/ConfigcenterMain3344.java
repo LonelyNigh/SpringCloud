d43fe28aa2d997c96d361fba2aa09a5ebdc2a25c
CY <CY@night.com>
2023-04-13 19:27:25 +0800

    3commit

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigcenterMain3344.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author ChengY
+ * @date 2023/4/13 19:25
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigcenterMain3344 {
+}
